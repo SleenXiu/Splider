86f72136931d4c2cd5c46e798adb521a7d0d3f84
sleen <xiushilin@hotmail.com>
2018-04-16 23:41:02 +0800

    add dbs

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 # coding:utf-8
 # author=xls
 
+from mongoengine import connect
+db = 'testdb'
+host = 'mongodb://127.0.0.1:27017/testdb'
+connect(alias=db, host=host)
 
 class App(object):
     def __init__(self):
